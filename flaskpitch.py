b491850a176e5c376a0e12c8779b244f33f16389
Zizoubrown <zizumoha2015@gmail.com>
2020-07-15 23:18:12 +0300

    add pitch variable

--- flaskpitch.py
+++ flaskpitch.py
@@ -1,9 +1,24 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+pitches = [
+    {
+        'author':'Abdul Aziz'
+        'title':'Pitch 1'
+        'content':'First Pitch content'
+        'date_posted':'July 14, 2020'
+    },
+    {
+        'author':'Anorld Swaz'
+        'title':'Pitch 2'
+        'content':'Second Pitch content'
+        'date_posted':'July 15, 2020'
+    }
+]
+
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', pitches=pitches)
 
 @app.route('/about')
 def about():
