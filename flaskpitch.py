9a34df570e931b52cda3b2c74e706dd429ca4846
Zizoubrown <zizumoha2015@gmail.com>
2020-07-15 23:10:36 +0300

    add templates

--- flaskpitch.py
+++ flaskpitch.py
@@ -1,13 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return '<h1>home page</h1>'
+    return render_template('home.html')
 
 @app.route('/about')
 def about():
-    return '<h1>about page</h1>'
+    return render_template('about.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
