836e2923710519369fb71ec8c22dadc8783e4e4e
Zizoubrown <zizumoha2015@gmail.com>
2020-07-22 15:32:27 +0300

    add post route and template to add pitch ideas t our website

--- app/routes.py
+++ app/routes.py
@@ -3,28 +3,15 @@ import secrets
 from PIL import Image
 from flask import render_template, url_for, flash, redirect, request
 from app import app, db, bcrypt
-from app.forms import RegistrationForm, LoginForm, UpdateAccountForm
+from app.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
 from app.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
 
-pitches = [
-    {
-        'author': 'Abdul Aziz',
-        'title':'Pitch 1',
-        'content':'First Pitch content',
-        'date_posted':'July 14, 2020'
-    },
-    {
-        'author':'Anorld Swaz',
-        'title':'Pitch 2',
-        'content':'Second Pitch content',
-        'date_posted':'July 15, 2020'
-    }
-]
 
 @app.route('/')
 def home():
-    return render_template('home.html', pitches=pitches)
+    posts = Post.query.all()
+    return render_template('home.html', posts=posts)
 
 @app.route('/about')
 def about():
@@ -97,3 +84,22 @@ def account():
         form.email.data = current_user.email
     image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
     return render_template('account.html', title='Account', image_file=image_file, form=form)
+
+
+@app.route('/post/new', methods= ['GET', 'POST'])
+@login_required
+def new_post():
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(title=form.title.data, content=form.content.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post has been created!', 'success')
+        return redirect(url_for('home'))
+    return render_template('preate_post.html', title='New Post', form=form)
+
+
+@app.route('/post/<int:post_id>'
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('post.html', title=post.title, post=post)
